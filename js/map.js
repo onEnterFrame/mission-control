62a87bcdc774fd1608f91495409af1d8ac717acc
Kingsley <james@builtclean.com>
2016-03-17 13:36:38 -0400

    add files back

--- js/map.js
+++ js/map.js
@@ -1,208 +1,247 @@
 var currentMission = 0;
-var missionProcessing =0
-var currentNode ={};
-function getMission(){
-	currentNode ={};
-	$.get( "partials/mission"+currentMission+"/M"+currentMission+"A.html", function( data ) {
-	  $(".details").html(data);
-	});
-	$(".icon_holder").css('background-image', "url('images/icons/mission.png')");
+var missionProcessing = 0
+var currentNode = {};
+
+var inProd = (location.hostname.indexOf('sap') > 1)
+
+function getMission() {
+    currentNode = {};
+    $.get("partials/mission" + currentMission + "/M" + currentMission + "A.html", function(data) {
+        $(".details").html(data);
+    });
+    $(".icon_holder").css('background-image', "url('images/icons/mission.png')");
 }
 
-function getNode(node){
-	currentNode = findNodeData(currentMission,currentMission+node)
-	$.get( "partials/mission"+currentMission+"/M"+currentMission+node+".html", function( data ) {
-	  $(".details").html(data);
-	});
-	$(".icon_holder").css('background-image', "url('images/icons/"+currentNode.icon.type+".png')");
-
-}
-
-function launchLink(){
-	if(currentNode.link){
-		window.open(currentNode.link)
-		pointsMe('M'+currentNode.id)
-	}
-	
-}
-
-
-function findNodeData(mission,nodeID){
-	var node_json
-	$.each(nodes_json.missions[mission]["mission"+mission].nodes, function(key, value){
-    if(value.id == nodeID){
-    	node_json =  value
+function getNode(node) {
+    currentNode = findNodeData(currentMission, currentMission + node)
+    $.get("partials/mission" + currentMission + "/M" + currentMission + node + ".html", function(data) {
+        $(".details").html(data);
+    });
+    if (icon.type != 'mission') {
+        $(".icon_holder").css('background-image', "url('images/icons/" + currentNode.icon.type + ".png')");
     }
-	});
-	return node_json
+
+}
+
+function launchLink() {
+    if (currentNode.link) {
+        window.open(currentNode.link)
+        pointsMe('M' + currentNode.id)
+    }
+
+}
+
+
+function findNodeData(mission, nodeID) {
+    var node_json
+    $.each(nodes_json.missions[mission]["mission" + mission].nodes, function(key, value) {
+        if (value.id == nodeID) {
+            node_json = value
+        }
+    });
+    return node_json
 }
 var user_records;
-function getUser(){
-	if(location.hostname.indexOf('sap') > 1){
-		loadData();
-		loadLeaderBoard()
-	}else{
-		loadFakeRecords()
-	}
-	 
+
+function getUser() {
+    if (inProd) {
+        loadData();
+        loadLeaderBoard()
+    } else {
+        loadFakeRecords()
+    }
+
+    $("#unlock_code").on('change keyup paste', function() {
+        if ($(this).val().length > 1) {
+            $(".unlock-btn").fadeTo(250, 1);
+        } else {
+            $(".unlock-btn").fadeTo(250, 0.5);
+        }
+    });
+
 }
 
 
- var theToken;
-  
-  $(document).ready(function(){
-  		GSCommunicator.setAppName('2045Future')
-  		GSCommunicator.fetchToken();
-  		//loadRecords();
-  		
+var theToken;
+
+$(document).ready(function() {
+    if (inProd) {
+        GSCommunicator.setAppName('2045Future')
+        GSCommunicator.fetchToken();
+    }
+
+
 })
 
-	function loadRecords(){
-  	GSCommunicator.getUserInformation(function (response) {
-		GSCommunicator.setPlayerId(response.email)
-	}, this);
-		 
-    	}
-   var loadAttempts = 0;
-  function loadData(){
-  	loadAttempts++
-  	if(loadAttempts > 18){
-  		$(".flow-wrapper").hide()
-  		alert("Sorry we could not find you in the database.");
-  		return;
-  	}
-		GSCommunicator.send( "getPlayerRecord" ,[GSCommunicator.getPlayerId()],  function( response ) {
-			if(response.error !=null){
-				setTimeout(function(){
-				loadRecords();
-				loadData();
-				},500)
-			
-			}else{
-				processRecords(response.result)
-			}
-			
-		}, this);
+function loadRecords() {
+    GSCommunicator.getUserInformation(function(response) {
+        GSCommunicator.setPlayerId(response.email)
+    }, this);
 
-  	}
+}
+var loadAttempts = 0;
 
-  function loadLeaderBoard(){
-		GSCommunicator.send( "getLeaderboard" ,["Experience Points", 1, 5, null, null, null],  function( response ) {
-			if(response.error !=null){
+function loadData() {
+    loadAttempts++
+    if (loadAttempts > 18) {
+        $(".flow-wrapper").hide()
+        alert("Sorry we could not find you in the database.");
+        return;
+    }
+    GSCommunicator.send("getPlayerRecord", [GSCommunicator.getPlayerId()], function(response) {
+        if (response.error != null) {
+            setTimeout(function() {
+                loadRecords();
+                loadData();
+            }, 500)
 
-			}else{
+        } else {
+            processRecords(response.result)
+        }
 
-				createLeaderBoard(response.result)
-			}
-		}, this);
+    }, this);
 
-  	}
+}
 
-  		var	theMap //= AdobeEdge.getComposition("EDGE-26179844").getStage().getSymbol("MapPanel").getSymbol("map")
-  	function processRecords(records){
-    	user_records = records;
-		$('.user-name').text(records.name)
-		$('.points').text(records.scores[0].amount)
-		var badges = records.earnedBadges
-		$.each(badges, function(i, item) {
-			var badge = $('.'+item.name)
-			if(i<3){
-			badge.css('display', 'inline').css('left', (i*50)+'px')
-			}else{
-			badge.css('display', 'inline').css('left', ((i-3)*50)+'px').css('top', '50px')
-			}
-		})
-		theMap = AdobeEdge.getComposition("EDGE-26179844").getStage().getSymbol("MapPanel").getSymbol("map")
-		processCompletedMissions()
-		processActiveMissions()
-		missionProcessing = 0;
-  	}
+function loadLeaderBoard() {
+    GSCommunicator.send("getLeaderboard", ["Experience Points", 1, 5, null, null, null], function(response) {
+        if (response.error != null) {
 
-  	function processActiveMissions(){
-  			var completedMissions = user_records.activeMissions
-		$.each(completedMissions, function(i, mission) {
+        } else {
 
-			missionProcessing = mission.name.match(/\d+/)[0]
-			$('.mission'+missionProcessing).show()
-			var missionID = "M"+mission.name.substr(7).toUpperCase()
-			try{
-				theMap.getSymbol(missionID).play(800)
-				var nodeData = findNodeData(missionProcessing, mission.name.substr(7).toUpperCase())
-				theMap.$(missionID).show()
-				theMap.getSymbol(missionID).$('glow').show()
-            	theMap.getSymbol(missionID).$('icon').css('background-image', "url('images/icons/"+nodeData.icon.type+"_wht.png')");
-            	if(!nodeData.required){
-            		theMap.getSymbol(missionID).$('icon').css('opacity', "0.4");
-            	}
-			}catch(e){
-				//console.log("no mission:",missionID, e)
-			}
+            createLeaderBoard(response.result)
+        }
+    }, this);
 
-		});
-  	}
+}
 
-  	function processCompletedMissions(){
-  			var completedMissions = user_records.completedMissions
-		$.each(completedMissions, function(i, mission) {
-			missionProcessing = mission.name.match(/\d+/)[0]
-			var missionID = "M"+mission.name.substr(7).toUpperCase()
-			try{
-				theMap.getSymbol(missionID).play(800)
-				var nodeData = findNodeData(missionProcessing, mission.name.substr(7).toUpperCase())
-				theMap.$(missionID).show()
-            	theMap.getSymbol(missionID).$('icon').css('background-image', "url('images/icons/"+nodeData.icon.type+"_"+nodeData.icon.color+".png')");
-			}catch(e){
-				//console.log("no mission:",missionID, e)
-			}
-			//
-			//console.log(missionID)
-		});
-  	}
+var theMap //= AdobeEdge.getComposition("EDGE-26179844").getStage().getSymbol("MapPanel").getSymbol("map")
+function processRecords(records) {
+    user_records = records;
+    $('.user-name').text(records.name)
+    $('.points').text(records.scores[0].amount)
+    var badges = records.earnedBadges
+    $.each(badges, function(i, item) {
+        var badge = $('.' + item.name)
+        if (i < 3) {
+            badge.css('display', 'inline').css('left', (i * 50) + 'px')
+        } else {
+            badge.css('display', 'inline').css('left', ((i - 3) * 50) + 'px').css('top', '50px')
+        }
+    })
+    theMap = AdobeEdge.getComposition("EDGE-26179844").getStage().getSymbol("MapPanel").getSymbol("map")
+    processCompletedMissions()
+    processActiveMissions()
+    missionProcessing = 0;
+}
 
-  	function loadFakeRecords(){
-  		$.ajax({
-		    url: 'js/getPlayerRecord.json',
-		    dataType: 'json',
-		    success: function(data) {
-		    processRecords(data)
-		    }
-		});
+function processActiveMissions() {
+    var completedMissions = user_records.activeMissions
+    $.each(completedMissions, function(i, mission) {
 
-		  $.ajax({
-		    url: 'js/leaderboard.json',
-		    dataType: 'json',
-		    success: function(data) {
-		    createLeaderBoard(data)
-		    }
-		});
+        missionProcessing = mission.name.match(/\d+/)[0]
+        $('.mission' + missionProcessing).show()
+        var missionID = "M" + mission.name.substr(7).toUpperCase()
+        try {
+            theMap.getSymbol(missionID).play(800)
+            var nodeData = findNodeData(missionProcessing, mission.name.substr(7).toUpperCase())
+            theMap.$(missionID).show()
+            theMap.getSymbol(missionID).$('glow').show()
+            theMap.getSymbol(missionID).$('icon').css('background-image', "url('images/icons/" + nodeData.icon.type + "_wht.png')");
+            if (!nodeData.required) {
+                theMap.getSymbol(missionID).$('icon').css('opacity', "0.4");
+            }
+        } catch (e) {
+            //console.log("no mission:",missionID, e)
+        }
 
-  	}
+    });
+}
 
+function processCompletedMissions() {
+    var completedMissions = user_records.completedMissions
+    $.each(completedMissions, function(i, mission) {
+        missionProcessing = mission.name.match(/\d+/)[0]
+        var missionID = "M" + mission.name.substr(7).toUpperCase()
+        try {
+            theMap.getSymbol(missionID).play(800)
+            var nodeData = findNodeData(missionProcessing, mission.name.substr(7).toUpperCase())
+            theMap.$(missionID).show()
+            theMap.getSymbol(missionID).$('icon').css('background-image', "url('images/icons/" + nodeData.icon.type + "_" + nodeData.icon.color + ".png')");
+        } catch (e) {
+            //console.log("no mission:",missionID, e)
+        }
+        //
+        //console.log(missionID)
+    });
+}
 
-  	function playVideo(){
-  		pointsMe('M'+currentNode.id)
-  		setTimeout(function(){$('.post-video').show();},1000)
-  		window.open('videos/video.html?M'+currentNode.id)
-  	}
+function loadFakeRecords() {
+    $.ajax({
+        url: 'js/getPlayerRecord.json',
+        dataType: 'json',
+        success: function(data) {
+            processRecords(data)
+        }
+    });
+
+    $.ajax({
+        url: 'js/leaderboard.json',
+        dataType: 'json',
+        success: function(data) {
+            createLeaderBoard(data)
+        }
+    });
+
+}
 
 
-function createLeaderBoard(data){
-	var leaderboard_html = "<table>"
-	$.each(data, function(i, user) {
-		leaderboard_html +="<tr><td width='45'>" +user.amount +"</td><td> "+user.displayName+"</td></tr>"
-	});
-	leaderboard_html +="</table>"
-	$('.leaderboard').html(leaderboard_html)
+function playVideo() {
+    pointsMe('M' + currentNode.id)
+    setTimeout(function() {
+        $('.post-video').show();
+    }, 1000)
+    window.open('videos/video.html?M' + currentNode.id)
+}
+
+
+function createLeaderBoard(data) {
+    var leaderboard_html = "<table>"
+    $.each(data, function(i, user) {
+        leaderboard_html += "<tr><td width='45'>" + user.amount + "</td><td> " + user.displayName + "</td></tr>"
+    });
+    leaderboard_html += "</table>"
+    $('.leaderboard').html(leaderboard_html)
 }
 
 
 
-  function pointsMe(node){
-  			GSCommunicator.send( "handleEvent" ,[{"siteId":"2045Future","type":"GetMission","playerid":GSCommunicator.getPlayerId(), "data":{"missionID":node}}],  function( response ) {
-			if(response.error !=null){
+function pointsMe(node) {
+    if (inProd) {
+        GSCommunicator.send("handleEvent", [{
+            "siteId": "2045Future",
+            "type": "GetMission",
+            "playerid": GSCommunicator.getPlayerId(),
+            "data": {
+                "missionID": node
+            }
+        }], function(response) {
+            if (response.error != null) {
 
-			}else{
-				loadData();
-			}
-		}, this);
-  }
+            } else {
+                loadData();
+            }
+        }, this);
+    }
+}
+
+
+function tryCode() {
+
+    if ($("#unlock_code").val().length > 0) {
+        //
+        alert("Code: " + $('#unlock_code').val() + " is not valid.\n\nStudy the past, if you would divine the future.\n ~ Confucius");
+        $("#unlock_code").val("");
+        $(".unlock-btn").fadeTo(250, 0.5);
+    }
+}
