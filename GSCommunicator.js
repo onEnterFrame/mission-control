a39d1c9f23f4d0f45efd1d9404033fbe3bbd4cf9
James Kingsley <james.kingsley@sap.com>
2016-04-12 17:14:13 +0000

    Updates from Varun

--- GSCommunicator.js
+++ GSCommunicator.js
@@ -18,8 +18,8 @@
     GSConfig.USER_API_URL = '/gamification/api/user/JsonRPC';
 
     // sometimes appropriate for API terminal 
-    GSConfig.TECH_API_URL = '/gamification/api/tech/JsonRPC';
-    
+    GSConfig.TECH_API_URL = '/gtech/JsonRPC';
+
     // session data is loaded from user data url (see SessionManager)
     GSConfig.USER_DATA_URL = '/gamification/api/user/UserData';
 
@@ -56,7 +56,7 @@
     var CURRENT_APP = GSConfig.DEFAULT_APP_NAME;
     var CURRENT_PLAYER_ID;
 
-   /**
+    /**
      * Sends an ajax POST request to the gamification service endpoints
      * 
      * @param method 
@@ -70,21 +70,21 @@
      * @param ajaxConfig 
      *      (optional) object holding $.ajax properties, to overwrite defaults 
      */
-    GSCommunicator.send = function( method, parameters, callbackFn, scope, ajaxConfig ) {
+    GSCommunicator.send = function(method, parameters, callbackFn, scope, ajaxConfig) {
         // defaults
         var bAsync = true;
         //var sUrl = GSConfig.USER_API_URL;
         var sUrl = '';
-         if(method == 'handleEvent'){
+        if (method == 'handleEvent') {
             sUrl = GSConfig.TECH_API_URL;
-        }else{
+        } else {
             sUrl = GSConfig.USER_API_URL;
         }
         var appName = CURRENT_APP;
         var timeout = 60000;
         var processData = true;
         var contentType = 'application/x-www-form-urlencoded; charset=UTF-8';
-        var dataType = null; 
+        var dataType = null;
         var xhrFields = {};
         var complexRequestData = (method && method.method) ? true : false;
         var httpMethod = 'POST';
@@ -92,37 +92,37 @@
 
         // ajax config is optional and can be used to overwrite $.ajax defaults
         if (ajaxConfig) {
-            if ( typeof ajaxConfig.async === 'boolean' ) {
+            if (typeof ajaxConfig.async === 'boolean') {
                 bAsync = ajaxConfig.async;
             }
 
             // check url
-            if ( typeof ajaxConfig.url === 'string' && ajaxConfig.url !== '' ) {
+            if (typeof ajaxConfig.url === 'string' && ajaxConfig.url !== '') {
                 sUrl = ajaxConfig.url;
             }
 
             // check method type
-            if ( typeof ajaxConfig.httpMethod === 'string' && validHttpMethods.indexOf(ajaxConfig.httpMethod) !== -1) {
+            if (typeof ajaxConfig.httpMethod === 'string' && validHttpMethods.indexOf(ajaxConfig.httpMethod) !== -1) {
                 httpMethod = ajaxConfig.httpMethod;
             }
 
             // check for provided app name
-            if ( typeof ajaxConfig.appName === 'string' && ajaxConfig.appName !== '' ) {
+            if (typeof ajaxConfig.appName === 'string' && ajaxConfig.appName !== '') {
                 appName = ajaxConfig.appName;
             }
 
             // check timeout
-            if ( (ajaxConfig.timeout % 1 === 0) && ajaxConfig.timeout > 0 ) {
+            if ((ajaxConfig.timeout % 1 === 0) && ajaxConfig.timeout > 0) {
                 timeout = ajaxConfig.timeout;
             }
 
             // check processData
-            if ( typeof ajaxConfig.processData === 'boolean' ) {
+            if (typeof ajaxConfig.processData === 'boolean') {
                 processData = ajaxConfig.processData;
             }
 
             // check content type
-            if ( (typeof ajaxConfig.contentType === 'boolean') || (typeof ajaxConfig.contentType === 'string') ) {
+            if ((typeof ajaxConfig.contentType === 'boolean') || (typeof ajaxConfig.contentType === 'string')) {
                 contentType = ajaxConfig.contentType;
             }
             // check data type
@@ -135,35 +135,35 @@
             }
         }
 
-        var ajaxData = null;        
+        var ajaxData = null;
         // prepare request context when available
         if (method && parameters) {
             var json = JSON.stringify({
-                method: complexRequestData ? method.method : method,   // json RPC methodname
-                params: parameters                 // paramteres for the assigned method
+                method: complexRequestData ? method.method : method, // json RPC methodname
+                params: parameters // paramteres for the assigned method
             });
-            if (! complexRequestData) {             // regular request
-            	ajaxData = {
-	    			json : json,
-	    			app : appName
-            	};
-            } else {                                // special case: request requires sending data via formdata (e.g. to allow adding files to the body)
-                ajaxData = new FormData();          // prepare FormData object, to send data and file in pairs
+            if (!complexRequestData) { // regular request
+                ajaxData = {
+                    json: json,
+                    app: appName
+                };
+            } else { // special case: request requires sending data via formdata (e.g. to allow adding files to the body)
+                ajaxData = new FormData(); // prepare FormData object, to send data and file in pairs
                 ajaxData.append('json', json);
                 ajaxData.append('app', appName);
                 // check for file
                 if (method.file) {
                     ajaxData.append('file', method.file);
                 }
-            }            
+            }
         }
 
         var headers = {};
         if (httpMethod === 'POST') {
             headers['X-CSRF-Token'] = CSRF_TOKEN;
         }
-		headers['X-CSRF-Token'] = CSRF_TOKEN;
-        return $.ajax( {
+        headers['X-CSRF-Token'] = CSRF_TOKEN;
+        return $.ajax({
             url: sUrl,
             data: (ajaxData) ? ajaxData : undefined,
             headers: headers,
@@ -175,32 +175,35 @@
             xhrFields: xhrFields, // used for native xhr / arraybuffer downloads
             async: bAsync, // avoid using false
             timeout: timeout, // default is 60000, can be greater for file uploads
-            success: function( _data, _textStatus, _jqXHR ) {
+            success: function(_data, _textStatus, _jqXHR) {
                 if (callbackFn) {
-                    callbackFn.call( scope ? scope : this, _data, _jqXHR );
+                    callbackFn.call(scope ? scope : this, _data, _jqXHR);
                 }
             },
-            error: function( jqXHR, textStatus, errorThrown ) {
+            error: function(jqXHR, textStatus, errorThrown) {
                 // when the token is invalid or already expired, then skip the successFn
                 // and call sendAPIRequest again to get a new token
-                if ( jqXHR.status === 403 && jqXHR.getResponseHeader( 'X-CSRF-Token' ) ) {
-                    return GSCommunicator.fetchToken( method, parameters, callbackFn, scope, ajaxConfig, sUrl );
+                if (jqXHR.status === 403 && jqXHR.getResponseHeader('X-CSRF-Token')) {
+                    return GSCommunicator.fetchToken(method, parameters, callbackFn, scope, ajaxConfig, sUrl);
                 }
-                console.error('error during backend request' );
+                console.error('error during backend request');
                 $("#error").html("<strong>Error</strong> error during backend request");
-                	$(".alert").show()
+                $(".alert").show()
             }
-        } );
+        });
     };
 
     /**
      * Requests information about the current user from the gamification service (name, roles,..)
      */
-    GSCommunicator.getUserInformation = function (callbackFn, scope) {
-    	// slightly reconfigure the default ajax request
-    	GSCommunicator.send(null, null, callbackFn, scope, { httpMethod: 'GET', url: GSConfig.USER_DATA_URL} );	
+    GSCommunicator.getUserInformation = function(callbackFn, scope) {
+        // slightly reconfigure the default ajax request
+        GSCommunicator.send(null, null, callbackFn, scope, {
+            httpMethod: 'GET',
+            url: GSConfig.USER_DATA_URL
+        });
     };
-    
+
     /**
      * Fetches a new csrf token from the backend.
      * This method is called, when the original request failed due to the missing xsrf token. 
@@ -208,40 +211,40 @@
      * and the original request is invoked egain. This call is always executed synchronously, as we expect it to 
      * be run only once and other requests should wait until the token is availbale.
      */
-    GSCommunicator.fetchToken = function( method, parameters, callbackFn, scope, ajaxConfig, JSONUrl ) {
-        return $.ajax( {
+    GSCommunicator.fetchToken = function(method, parameters, callbackFn, scope, ajaxConfig, JSONUrl) {
+        return $.ajax({
             url: GSConfig.USER_API_URL,
             headers: {
                 'X-CSRF-Token': 'Fetch'
             },
             type: 'GET',
             async: false,
-            success: function (data, textStatus, jqXHR) {
-                var headerToken = jqXHR.getResponseHeader( 'X-CSRF-Token' );
-                if ( headerToken !== null ) {
+            success: function(data, textStatus, jqXHR) {
+                var headerToken = jqXHR.getResponseHeader('X-CSRF-Token');
+                if (headerToken !== null) {
                     CSRF_TOKEN = headerToken;
                     theToken = CSRF_TOKEN;
                     // if there was a request running and interrupted due to missing token, repeat it
                     if (method) {
-                        return GSCommunicator.send( method, parameters, callbackFn, scope, ajaxConfig );    
+                        return GSCommunicator.send(method, parameters, callbackFn, scope, ajaxConfig);
                     }
                 } else {
                     console.error("Could not fetch CSRF token.");
                 }
             },
-            error: function ( jqXHR, textStatus, errorThrown) {
-            	var headerToken = jqXHR.getResponseHeader( 'X-CSRF-Token' );
-                if ( headerToken !== null ) {
+            error: function(jqXHR, textStatus, errorThrown) {
+                var headerToken = jqXHR.getResponseHeader('X-CSRF-Token');
+                if (headerToken !== null) {
                     CSRF_TOKEN = headerToken;
                     // if there was a request running and interrupted due to missing token, repeat it
                     if (method) {
-                        return GSCommunicator.send( method, parameters, callbackFn, scope, ajaxConfig );    
+                        return GSCommunicator.send(method, parameters, callbackFn, scope, ajaxConfig);
                     }
                 } else {
                     console.error('Could not fetch CSRF token.');
                 }
             }
-        } );
+        });
     };
 
     /**
@@ -250,7 +253,7 @@
      * Current App = active app.
      * Current App != default app.
      */
-    GSCommunicator.setAppName = function (appName) {
+    GSCommunicator.setAppName = function(appName) {
         if (appName && typeof appName === 'string') {
             CURRENT_APP = appName;
         } else {
@@ -261,7 +264,7 @@
     /**
      * Returns the name of the currently active app.
      */
-    GSCommunicator.getAppName = function () {
+    GSCommunicator.getAppName = function() {
         return CURRENT_APP;
     };
 
@@ -270,7 +273,7 @@
      * automatically but serves as a convenience storage for a player id. Note: user and player can be 
      * the same but do not have to be the same, e.g. when a player views a profile of another player.
      */
-    GSCommunicator.setPlayerId = function (playerId) {
+    GSCommunicator.setPlayerId = function(playerId) {
         if (playerId && typeof playerId === 'string') {
             CURRENT_PLAYER_ID = playerId;
         } else {
@@ -281,7 +284,7 @@
     /**
      * Returns the id of the currently used player.
      */
-    GSCommunicator.getPlayerId = function () {
+    GSCommunicator.getPlayerId = function() {
         return CURRENT_PLAYER_ID;
     };
 
